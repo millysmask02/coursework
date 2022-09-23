9a36b4deff4ce3eb9395af4fcce7dc360b61aa84
Манана <76215627+millysmask02@users.noreply.github.com>
2022-09-23 19:27:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 using namespace sf;
 int cellSize = 105;
 Vector2i offset(35, 35);
+int sizeLevel = 5;
 
 struct piece {
 	int x, y; // координаты точки
@@ -38,9 +39,9 @@ struct piece {
 
 // Распределение цветов (типов лапок) и запись координат
 void completion() {
-	for (int i = 1; i <= 5; i++) {
-		for (int j = 1; j <= 5; j++) {
-			grid[i][j].kind = rand() % 4;
+	for (int i = 1; i <= sizeLevel; i++) {
+		for (int j = 1; j <= sizeLevel; j++) {
+			grid[i][j].kind = rand() % (sizeLevel - 1);
 			grid[i][j].col = j;
 			grid[i][j].row = i;
 			grid[i][j].x = offset.x + (j - 1) * cellSize;
@@ -53,8 +54,8 @@ void completion() {
 //Match finding поиск совпадений 
 //*т.е находим все нужные 3-ки и помечаем каждую как помеченную*/
 void matchFinding() {
-	for (int i = 1; i <= 5; i++) {
-		for (int j = 1; j <= 5; j++) {
+	for (int i = 1; i <= sizeLevel; i++) {
+		for (int j = 1; j <= sizeLevel; j++) {
 			if (grid[i][j].kind == grid[i + 1][j].kind) {
 				if (grid[i][j].kind == grid[i - 1][j].kind) {
 					for (int n = -1; n <= 1; n++) {
@@ -73,13 +74,14 @@ void matchFinding() {
 	}
 }
 
+
 int main()
 {
 	srand(time(0));
 	RenderWindow window(VideoMode(900, 600), "3 in a row with kittens");
 	window.setFramerateLimit(60);
 
-	// Фон
+	// Фон уровень 1
 	Image imBackgroundLevel1;
 	imBackgroundLevel1.loadFromFile("images/lvl1.jpg");
 
@@ -88,6 +90,26 @@ int main()
 
 	Sprite sprBackgroundLevel1;
 	sprBackgroundLevel1.setTexture(textBackgroundLevel1);
+
+	// Фон уровень 2
+	Image imBackgroundLevel2;
+	imBackgroundLevel2.loadFromFile("images/lvl2.jpg");
+
+	Texture textBackgroundLevel2;
+	textBackgroundLevel2.loadFromImage(imBackgroundLevel2);
+
+	Sprite sprBackgroundLevel2;
+	sprBackgroundLevel2.setTexture(textBackgroundLevel2);
+
+	// Фон уровень 3
+	Image imBackgroundLevel3;
+	imBackgroundLevel3.loadFromFile("images/lvl3.jpg");
+
+	Texture textBackgroundLevel3;
+	textBackgroundLevel3.loadFromImage(imBackgroundLevel3);
+
+	Sprite sprBackgroundLevel3;
+	sprBackgroundLevel3.setTexture(textBackgroundLevel3);
 
 	// Сетка
 	Image imSquareLevel1;
@@ -120,9 +142,8 @@ int main()
 	Texture textLabel;
 	textLabel.loadFromImage(imLabel);
 
-	Sprite sprLabel, sprSmallLabels;
+	Sprite sprLabel;
 	sprLabel.setTexture(textLabel);
-	sprSmallLabels.setTexture(textLabel);
 
 	// Шрифт
 	Font font;
@@ -161,7 +182,8 @@ int main()
 	bool isMoving = false;
 	int points = 0; // кол-во удаленных фишек
 	bool win = false; // флаг, чтобы не давать ничего делать после победы
-	bool start_click = false;
+	int start_click = 0;
+	int level = 0;
 
 
 	while (window.isOpen()) {
@@ -179,13 +201,18 @@ int main()
 					if ((Mouse::getPosition(window).x >= 600) && // для кнопки mix
 						(Mouse::getPosition(window).x <= 850) &&
 						(Mouse::getPosition(window).y >= 455) &&
-						(Mouse::getPosition(window).y <= 540) && !win)  {
+						(Mouse::getPosition(window).y <= 540) && !win) {
 							completion();
+					}
+					if ((Mouse::getPosition(window).x >= 325) && // для кнопки next
+						(Mouse::getPosition(window).x <= 575) &&
+						(Mouse::getPosition(window).y >= 420) &&
+						(Mouse::getPosition(window).y <= 520) && level == 0) {
+						start_click++;
 					}
 				}
 			}
 		}
-
 		
 		// Кликанье - обмен
 		if (click == 1) {
@@ -208,8 +235,8 @@ int main()
 		matchFinding();
 
 		isMoving = 0;
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= 5; j++) {
+		for (int i = 1; i <= sizeLevel; i++) {
+			for (int j = 1; j <= sizeLevel; j++) {
 				piece& p = grid[i][j];
 				int dx, dy;
 				for (int n = 0; n < 4; n++) {  // 4 - speed
@@ -264,8 +291,8 @@ int main()
 		
 		//Update grid
 		if (!isMoving) {
-			for (int i = 5; i > 0; i--) {
-				for (int j = 1; j <= 5; j++) {
+			for (int i = sizeLevel; i > 0; i--) {
+				for (int j = 1; j <= sizeLevel; j++) {
 					if (grid[i][j].match) {
 						for (int n = i; n > 0; n--) {
 							if (!grid[n][j].match) {
@@ -277,11 +304,11 @@ int main()
 				}
 			}
 			
-			for (int j = 1; j <= 5; j++) {
-				for (int i = 5, n = 0; i > 0; i--) {
+			for (int j = 1; j <= sizeLevel; j++) {
+				for (int i = sizeLevel, n = 0; i > 0; i--) {
 					if (grid[i][j].match) {
 						points += 10;
-						grid[i][j].kind = rand() % 4;
+						grid[i][j].kind = rand() % (sizeLevel - 1);
 						grid[i][j].y = -cellSize * n++;
 						grid[i][j].match = 0;
 						//grid[i][j].alpha = 255;
@@ -289,8 +316,9 @@ int main()
 				}
 			} 
 		}
-		if (points > 3000) {
+		if (points > 1000) {
 			win = true;
+			start_click = 0;
 		}
 
 		window.clear();
@@ -298,27 +326,75 @@ int main()
 		window.draw(sprBackgroundLevel1);
 
 		
-		if (!click) {
+		if (start_click == 0 && level == 0) {
 			sprLabel.setScale(Vector2f(2, 3));
-			sprLabel.setPosition(200, 150);
+			sprLabel.setPosition(200, 100);
 			window.draw(sprLabel);
 			textLevel.setString(" Find out the story\nof two cats by\ncompleting the task \nof levels");
-			textLevel.setPosition(210, 155);
+			textLevel.setPosition(210, 105);
+			window.draw(textLevel);
+			
+		}
+
+		if ((start_click == 0 && level == 0) ||
+			(start_click == 1) || (win)) {
+			sprLabel.setScale(Vector2f(1, 1));
+			sprLabel.setPosition(325, 420);
+			window.draw(sprLabel);
+			textLevel.setString("next");
+			textLevel.setPosition(390, 435);
 			window.draw(textLevel);
 		}
 
-		if (click) {
+		if (start_click == 1) {
+			sprLabel.setScale(Vector2f(2, 3));
+			sprLabel.setPosition(200, 100);
+			window.draw(sprLabel);
+			if (level == 0) {
+				// история 1
+			}
+			if (level == 1) {
+				// история 2
+			}
+			if (level == 2) {
+				// история 3
+			}
+		}
+
+		if (win) {
+			sprLabel.setScale(Vector2f(2, 3));
+			sprLabel.setPosition(200, 100);
+			window.draw(sprLabel);
+			textLevel.setString("какой-то текст");
+			textLevel.setPosition(390, 435);
+			window.draw(textLevel);
+		}
+
+		if (start_click == 2) {
+			level++;
+		}
+
+		if (level > 0 && !win) {
 			// Рисуем сетку
-			for (int i = 0; i < 5; i++) {
-				for (int j = 0; j < 5; j++) {
-					sprSquareLevel1.setPosition(35 + 105 * i, 35 + 105 * j);
+			for (int i = 0; i < sizeLevel; i++) {
+				for (int j = 0; j < sizeLevel; j++) {
+					switch (sizeLevel) {
+					case 5: 
+						sprSquareLevel1.setPosition(35 + 105 * i, 35 + 105 * j);
+					case 6:
+						sprSquareLevel1.setPosition(35 + 89 * i, 35 + 89 * j);//84
+					case 7:
+						sprSquareLevel1.setPosition(35 + 77 * i, 35 + 77 * j);//72
+					default:
+						break;
+					}
 					window.draw(sprSquareLevel1);
 				}
 			}
 
 			// Рисуем лапки
-			for (int i = 1; i <= 5; i++) {
-				for (int j = 1; j <= 5; j++) {
+			for (int i = 1; i <= sizeLevel; i++) {
+				for (int j = 1; j <= sizeLevel; j++) {
 					piece p = grid[i][j];
 					sprLapa = colorLapa[p.kind];
 					//sprLapa.setColor(colorLapa[p.kind]);
@@ -365,10 +441,10 @@ int main()
 			}
 			window.draw(textLevel);
 
-			sprSmallLabels.setScale(Vector2f(1, 0.85));
+			sprLabel.setScale(Vector2f(1, 0.85));
 
-			sprSmallLabels.setPosition(600, 455);
-			window.draw(sprSmallLabels);
+			sprLabel.setPosition(600, 455);
+			window.draw(sprLabel);
 			textLevel.setString("MIX");
 			textLevel.setPosition(670, 465);
 			window.draw(textLevel);
